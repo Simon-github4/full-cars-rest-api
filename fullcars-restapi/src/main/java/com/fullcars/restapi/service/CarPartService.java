552d7d83dd1b85e50da756ba129c1306b04c59b3
Simon-github4 <simon.diaz@ufasta.edu.ar>
2025-06-25 09:30:30 -0300

    sku fix

--- fullcars-restapi/src/main/java/com/fullcars/restapi/service/CarPartService.java
+++ fullcars-restapi/src/main/java/com/fullcars/restapi/service/CarPartService.java
@@ -4,8 +4,11 @@ import java.util.List;
 
 import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
 import com.fullcars.restapi.model.CarPart;
 import com.fullcars.restapi.repository.ICarPartRepository;
+
 import jakarta.persistence.EntityNotFoundException;
 
 @Service
@@ -17,10 +20,17 @@ private ICarPartRepository carPartRepo;
 		this.carPartRepo = repo;
 	}
 	
+	@Transactional
 	public CarPart save(CarPart c) {
-		return carPartRepo.save(c);
+		CarPart part = carPartRepo.save(c);
+		
+		if (part.getSku() == null || part.getSku().isBlank()) { 
+            part.setSku(generateSku(part));
+			part = carPartRepo.save(part);
+		}
+		return part;
 	}
-	
+
 	public void delete(Long id) {
         if (!carPartRepo.existsById(id)) 
             throw new EntityNotFoundException("Auto Parte no encontrada con id: " + id);
@@ -37,7 +47,20 @@ private ICarPartRepository carPartRepo;
 						new EntityNotFoundException("Auto Parte no encontrada con sku: " + sku));
 	}
 	
+	@Transactional(readOnly = true)
 	public List<CarPart> getCarParts(){
 		return carPartRepo.findAll(Sort.by(Sort.Direction.DESC, "id"));
 	}
+	
+	private String generateSku(CarPart c) {
+		StringBuilder sb = new StringBuilder();
+		sb.append(getSafePrefix(c.getBrand().getName())).append("-")
+		  .append(getSafePrefix(c.getCategory().getName())).append("-")
+		  .append(getSafePrefix(c.getName())).append("-")
+		  .append(c.getId().toString());
+		return sb.toString();
+	}
+	private String getSafePrefix(String value) {
+        return value == null ? "XXX" : value.replaceAll("[^A-Za-z]", "").toUpperCase().substring(0, Math.min(3, value.length()));
+    }
 }
